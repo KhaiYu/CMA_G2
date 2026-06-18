73be8c7df9092ec9821e0c6cb2be5bc5465b0489
Yong Qi <turddude05@gmail.com>
2026-06-18 22:34:54 +0800

    Update q_func.m

--- q_func.m
+++ q_func.m
@@ -1,10 +1,8 @@
 function y = q_func(x, arr, st)
 
-  %calculates how many patients are in the queue at every time poinnt 'x'
   y = zeros(1, length(x));
 
   for i = 1:length(x)
-   % patients arrived but haven't started service yet
     y(i) = sum((arr <= x(i)) & (st > x(i)));
   end
 endfunction
